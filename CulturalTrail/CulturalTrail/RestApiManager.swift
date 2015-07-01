a3e7ef6a5743a8c0ff71a2c1cf6d72e545ae7dc0
Naush <nkorai@purdue.edu>
2015-06-30 21:13:29 -0400

    Users can now see a list of all issues (just titles)

---
+++ CulturalTrail/CulturalTrail/RestApiManager.swift
@@ -0,0 +1,36 @@
+//
+//  RestApiManager.swift
+//  CulturalTrail
+//
+//  Created by Nausherwan Korai on 6/30/15.
+//  Copyright (c) 2015 Indy Cultural Trail. All rights reserved.
+//
+
+import Foundation
+
+typealias ServiceResponse = (JSON, NSError?) -> Void
+
+class RestApiManager: NSObject {
+    static let sharedInstance = RestApiManager()
+    
+    let baseURL = "http://culturaltrail.herokuapp.com/issues"
+    
+    func getIssues(onCompletion: (JSON) -> Void) {
+        let route = baseURL
+        makeHTTPGetRequest(route, onCompletion: { json, err in
+            onCompletion(json as JSON)
+        })
+    }
+    
+    func makeHTTPGetRequest(path: String, onCompletion: ServiceResponse) {
+        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            let json:JSON = JSON(data: data)
+            onCompletion(json, error)
+        })
+        task.resume()
+    }
+}
--- CulturalTrail/CulturalTrail/ViewController.swift
+++ CulturalTrail/CulturalTrail/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableView:UITableView?
+    var items = NSMutableArray()
+    
+    override func viewWillAppear(animated: Bool) {
+        let frame:CGRect = CGRect(x: 0, y: 100, width: self.view.frame.width, height: self.view.frame.height-100)
+        self.tableView = UITableView(frame: frame)
+        self.tableView?.dataSource = self
+        self.tableView?.delegate = self
+        self.view.addSubview(self.tableView!)
+        
+        let btn = UIButton(frame: CGRect(x: 0, y: 25, width: self.view.frame.width, height: 50))
+        btn.backgroundColor = UIColor.cyanColor()
+        btn.setTitle("Add new Dummy", forState: UIControlState.Normal)
+        btn.addTarget(self, action: "addDummyData", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(btn)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addDummyData() {
+        NSLog("PRESS")
+        RestApiManager.sharedInstance.getIssues { json in
+            let results = json
+            
+            for (index: String, subJson: JSON) in results {
+                let issue: AnyObject = subJson.object
+                self.items.addObject(issue)
+                dispatch_async(dispatch_get_main_queue(),{
+                    tableView?.reloadData()
+                })
+            }
+            NSLog("%d",self.items.count)
+        }
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("CELL") as? UITableViewCell
+        
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "CELL")
+        }
+        
+        let issue:JSON =  JSON(self.items[indexPath.row])
+        
+        //let picURL = issue["picture"]["medium"].string
+        //let url = NSURL(string: picURL!)
+        //let data = NSData(contentsOfURL: url!)
+        
+        cell!.textLabel?.text = issue["name"].string
+        //cell?.imageView?.image = UIImage(data: data!)
+        
+        return cell!
+    }
 }
-
